a9c241a6de4120eb606d93cb3452943f49857895
Oscar Bautista <oscar04262000@gmail.com>
2021-05-11 21:01:54 -0500

    verificacion de usuario y clave

--- controlador/userC.php
+++ controlador/userC.php
@@ -3,11 +3,11 @@
     class UserC{
         public function IngresoC(){
             $user = $_POST["user"];
-            
+            $pass = $_POST["pass"];
             $_SESSION["user"] = $user;
             $repuesta = UserM::IngresoM($user);
 
-            if($repuesta["Nombre"] == $user){
+            if($repuesta["Nombre"] == $user && $repuesta["clave"] == $pass){
                 header("location:../../../vistas/panel.php");
             }
             
